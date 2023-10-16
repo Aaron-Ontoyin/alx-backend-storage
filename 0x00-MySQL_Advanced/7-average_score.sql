1552b5f328e88406c87689f2fda0483c823eae68
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-16 23:53:32 +0000

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,13 +5,14 @@
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE average_score_calc FLOAT;
-
-    SELECT AVG(score) INTO average_score_calc FROM corrections WHERE user_id = user_id;
-
-    UPDATE users SET average_score = average_score_calc WHERE id = user_id;
+	UPDATE
+        users
+	SET 
+        average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE
+        id = user_id;
 END;
 //
 
