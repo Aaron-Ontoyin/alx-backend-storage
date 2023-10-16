df77c0d3f9ea8824e938ad300ff47c81b9a51fd6
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-16 22:47:23 +0000

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -3,24 +3,12 @@
 
 SOURCE metal_bands.sql;
 
-CREATE TEMPORARY TABLE glam_rock_bands AS
-SELECT band_name,
-       CAST(SUBSTRING_INDEX(lifespan, '-', 1) AS UNSIGNED) AS start_year,
-       CAST(SUBSTRING_INDEX(lifespan, '-', -1) AS UNSIGNED) AS end_year
-FROM metal_bands
-WHERE main_style = 'Glam rock';
-
-UPDATE glam_rock_bands
-SET end_year = IFNULL(end_year, 2022);
-
-UPDATE glam_rock_bands
-SET lifespan = end_year - start_year;
-
-SELECT band_name, lifespan
-FROM glam_rock_bands
-ORDER BY lifespan DESC;
-
-SELECT origin, COUNT(DISTINCT nb_fans) AS total_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY total_fans DESC;
+SELECT 
+    origin,
+    SUM(fans) AS nb_fans
+FROM 
+    metal_bands
+GROUP BY 
+    origin
+ORDER BY 
+    nb_fans DESC;
