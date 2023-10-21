c573ff653febc1009d1be1cb4dbb794fb07954ad
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-21 02:41:32 +0000

    task 12

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -14,19 +14,6 @@ def log_stats(mongo_collection):
 
     print("{} status check".format(mongo_collection.count_documents({"method": "GET", "path": "/status"})))
 
-    # Top 10 IPs
-    top_ips = mongo_collection.aggregate([
-        {"$group": {
-            "_id": "$ip",
-            "count": {"$sum": 1}
-        }},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ])
-
-    print("IPs:")
-    for ip in top_ips:
-        print("\t{}: {}".format(ip["_id"], ip["count"]))
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
